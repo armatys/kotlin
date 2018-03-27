0861b3df0b5fed68ebbb07059b15181769fa5efe
Dmitry Savvinov <dsavvinov@gmail.com>
2018-03-27 13:41:03 +0300

    Add support of internal arguments for language feature settings
    Arguments are passed in form '-XX:+LanguageFeatureName' for enabling
    LanguageFeature.LanguageFeatureName, and '-XX:-LanguageFeatureName' for
    disabling.
    
    Note that they do override other settings, including 'language-version'
    or extra ('-X') args.

--- compiler/tests-common/tests/org/jetbrains/kotlin/checkers/CompilerTestLanguageVersionSettings.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/checkers/CompilerTestLanguageVersionSettings.kt
@@ -42,6 +42,8 @@ data class CompilerTestLanguageVersionSettings(
     override fun getFeatureSupport(feature: LanguageFeature): LanguageFeature.State =
             languageFeatures[feature] ?: delegate.getFeatureSupport(feature)
 
+    override fun isPreRelease(): Boolean = false
+
     @Suppress("UNCHECKED_CAST")
     override fun <T> getFlag(flag: AnalysisFlag<T>): T = analysisFlags[flag] as T? ?: flag.defaultValue
 }
